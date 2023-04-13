d4311ee190fcd2b9c4bf0dcc242f9bb338eea1ae
assia04 <130609077+assia04@users.noreply.github.com>
2023-04-13 11:55:56 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+
+namespace UA3Projet
+{
+    public class Client
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Courriel { get; set; }
+        public string CarteDeCredit { get; set; }
+        public int NumeroDeChambre { get; set; }
+
+        public Client(string nom, string prenom, string courriel, string carteDeCredit, int numeroDeChambre)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Courriel = courriel;
+            CarteDeCredit = carteDeCredit;
+            NumeroDeChambre = numeroDeChambre;
+        }
+    }
+
+    public class Hotel
+    {
+        public ArrayList Clients { get; set; }
+
+        public Hotel()
+        {
+            Clients = new ArrayList();
+        }
+
+        public void AjouterClient(Client client)
+        {
+            Clients.Add(client);
+        }
+    }
+
+}
